68255e096e97c27a6da22f1b37c698490bb20969
khalvat-M <mmba313@gmail.com>
2023-06-22 15:49:45 +0330

    Revert "DO NOT MERGE CameraManager: Provide flag for overriding camera output to portrait."
    This reverts commit ddc70c75f35de7be6752b9138574445216c3aa0c.
    
    (cherry picked from commit 7a8cf57eef32de2dbcde2084974dcbc4417c6f1b)

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -29,7 +29,6 @@ import android.annotation.SdkConstant;
 import android.annotation.SdkConstant.SdkConstantType;
 import android.app.ActivityThread;
 import android.app.AppOpsManager;
-import android.app.compat.CompatChanges;
 import android.compat.annotation.UnsupportedAppUsage;
 import android.content.Context;
 import android.content.res.Resources;
@@ -37,7 +36,6 @@ import android.graphics.ImageFormat;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.SurfaceTexture;
-import android.hardware.camera2.CameraManager;
 import android.media.AudioAttributes;
 import android.media.IAudioService;
 import android.os.Build;
@@ -317,13 +315,6 @@ public class Camera {
         return numberOfCameras;
     }
 
-    private static final boolean sLandscapeToPortrait =
-            SystemProperties.getBoolean(CameraManager.LANDSCAPE_TO_PORTRAIT_PROP, false);
-
-    private static boolean shouldOverrideToPortrait() {
-        return CompatChanges.isChangeEnabled(CameraManager.OVERRIDE_FRONT_CAMERA_APP_COMPAT)
-                && sLandscapeToPortrait;
-    }
 
     /**
      * Returns the number of physical cameras available on this device.
@@ -344,9 +335,7 @@ public class Camera {
         if (cameraId >= getNumberOfCameras()) {
             throw new RuntimeException("Unknown camera ID");
         }
-        boolean overrideToPortrait = shouldOverrideToPortrait();
-
-        _getCameraInfo(cameraId, overrideToPortrait, cameraInfo);
+        _getCameraInfo(cameraId, cameraInfo);
         IBinder b = ServiceManager.getService(Context.AUDIO_SERVICE);
         IAudioService audioService = IAudioService.Stub.asInterface(b);
         try {
@@ -359,8 +348,7 @@ public class Camera {
             Log.e(TAG, "Audio service is unavailable for queries");
         }
     }
-    private native static void _getCameraInfo(int cameraId, boolean overrideToPortrait,
-            CameraInfo cameraInfo);
+    private native static void _getCameraInfo(int cameraId, CameraInfo cameraInfo);
 
     /**
      * Information about a camera
@@ -541,9 +529,8 @@ public class Camera {
             mEventHandler = null;
         }
 
-        boolean overrideToPortrait = shouldOverrideToPortrait();
         return native_setup(new WeakReference<Camera>(this), cameraId,
-                ActivityThread.currentOpPackageName(), overrideToPortrait);
+                ActivityThread.currentOpPackageName());
     }
 
     /** used by Camera#open, Camera#open(int) */
@@ -616,8 +603,7 @@ public class Camera {
     }
 
     @UnsupportedAppUsage
-    private native int native_setup(Object cameraThis, int cameraId, String packageName,
-            boolean overrideToPortrait);
+    private native int native_setup(Object cameraThis, int cameraId, String packageName);
 
     private native final void native_release();
 
